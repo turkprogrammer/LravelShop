14fe8f3f3eeec95b3744ef57b54a485eb30b1da6
TurkProgrammer <ostbat43@gmail.com>
2020-09-28 02:50:04 +0500

    migrations, seeder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
     //
     public function index(){
+        $products = Product::all();
+        dd($products);
         return view('home.index');
     }
 }
