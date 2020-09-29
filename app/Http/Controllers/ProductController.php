aba410a5a4cfefe5e5aaef73c701c9403619a967
TurkProgrammer <ostbat43@gmail.com>
2020-09-30 01:05:41 +0500

    Категории товаров передаю во все вьюшки с помощью AppServiceProvider (Sharing Data With All Views)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,6 +13,9 @@ class ProductController extends Controller
     public function show($cat, $product_id){
         
         $item = \App\Product::where('id', $product_id)->first();
+        //$categories = \App\Category::all();
+       
+        
         return view('product.show',[
             'item' =>$item
         ]);
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        $categories = \App\Category::orderBy('id')->get(); // $categories pass to all views
+        View::share([
+            'categories' =>$categories
+        ]);
     }
 }
